639c9bf857375f6cc820432754177be9ee3d84f8
Masaya Nasu <tomato.wonder.life@gmail.com>
2024-04-07 00:19:03 +0900

    Update frontend/src/components/common/editor/plugins/LinkPlugin.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/components/common/editor/plugins/LinkPlugin.ts
+++ frontend/src/components/common/editor/plugins/LinkPlugin.ts
@@ -4,6 +4,7 @@ import { $isLinkNode } from "@lexical/link";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import type { LexicalEditor } from "lexical";
 import { $getNearestNodeFromDOMNode } from "lexical";
+import { useEffect } from "react";
 import { BrowserOpenURL } from "../../../../../wailsjs/runtime";
 
 export default function LinkPlugin() {
